a4642678ea7da3b3cfe39ef0544bfd577b260c07
ronanc02 <chesnelr@gmail.com>
2022-09-03 18:19:00 +0200

    Implement jetpack nav component

--- app/src/main/java/com/example/koreanlearning/MainActivity.kt
+++ app/src/main/java/com/example/koreanlearning/MainActivity.kt
@@ -13,30 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        Inflate without fragments
-//        setContentView(R.layout.main)
 
         // Inflate with fragments
         val binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Initialize data.
-//        val json = Json(this).parseJson()
-//        val myDataset = Datasource(this, json).loadAffirmations()
-//        val myDataset = json?.let { Datasource(this, it).loadAffirmations() }
-
-//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//        recyclerView.adapter = ItemAdapter(this, myDataset)
-//        recyclerView.adapter = myDataset?.let { ItemAdapter(this, it) }
-
-//        recyclerView.layoutManager = GridLayoutManager(this, 2)
-
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
- //       recyclerView.setHasFixedSize(true)
-
-        //println(Json(this).parseJson().toString())
-        //Log.i("data", Json(this).parseJson().toString())
 
     }
 }
